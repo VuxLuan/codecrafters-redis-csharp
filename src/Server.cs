e9a09ccf4689d6bf46a87cd751ddab922bb57ddf
VuxLuan <luanvu@MBL.local>
2024-12-06 10:29:35 +0700

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -5,6 +5,23 @@ using System.Net.Sockets;
 Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
+var server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+while (true) {
+    try {
+        Socket clientSocket = await server.AcceptSocketAsync();
+        _ = Task.Run(() => HandleRequestAsync(clientSocket));
+    } catch (Exception ex) {
+        Console.WriteLine(ex);
+        throw;
+    }
+}
+
+async Task HandleRequestAsync(Socket socket)
+{
+   //var buffer = new byte[1024];
+    //var bytesRead = await socket.ReceiveAsync(buffer, SocketFlags.None);
+    await socket.SendAsync(new ArraySegment<byte>("+PONG\r\n"u8.ToArray()), SocketFlags.None);
+}
+
+
